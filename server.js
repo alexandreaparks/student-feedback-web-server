0c7d11558fd54bdf2fb29707d3b77b27b37f7f4f
alexandrea parks <alparks@idk.com>
2024-05-02 20:35:37 -0500

    layout.hbs and styles

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express()  // create web application server
 //enable parsing of POST request form body
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// server static files such as css
+const staticFileLocation = path.join(__dirname, 'public')  // where static files are stored
+app.use(express.static(staticFileLocation))  // tell application to use static files and where they can be found
+
 // configure application to use Handlebars template engine and work with template files in views directory
 // in other words - tell application where views (HTML/template files) are and use Handlebars to generate them
 app.set('views', path.join(__dirname, 'views'))  // __dirname is where the project is stored
